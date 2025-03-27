7360ca75a7a84d3f888fd47cd3693e54af71814f
mindex <dev@mindex.com>
2025-03-27 18:31:19 -0400

    unimplemented ReportingStructure class with required fields

---
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -0,0 +1,14 @@
+package com.mindex.challenge.data;
+
+/**
+ * Determines the number of direct and indirect reports of a given employee upon instantiation. 
+ * 
+ */
+public class ReportingStructure {
+    private Employee employee;
+    private int numberOfReports;
+
+    public ReportingStructure(Employee employee) {
+        
+    }
+}
