34fe177e76c858f63e16912e9a4ce3a887002ab2
mindex <dev@mindex.com>
2025-03-27 23:50:24 -0400

    REST endpoint implementation, in progress

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,16 +1,20 @@
 package com.mindex.challenge.controller;
 
-import com.mindex.challenge.data.Employee;
-import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
@@ -39,5 +43,19 @@ public class EmployeeController {
 
         employee.setEmployeeId(id);
         return employeeService.update(employee);
+    }
+
+    //Include try/catch statement and ResponseEntity return value for error handling in case of invalid ID or server error
+    @GetMapping("/employee/reports/{id}")
+    public ResponseEntity<?> reportingStructure(@PathVariable String id) {
+        try {
+            Employee employee = employeeService.read(id);
+            ReportingStructure reportingStructure = new ReportingStructure(employee);
+            return new ResponseEntity<>(reportingStructure, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
