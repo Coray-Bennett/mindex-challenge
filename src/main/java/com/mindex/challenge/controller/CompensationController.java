b40663fbc6ca2617d16d8eb128b6433ad93eac6d
mindex <dev@mindex.com>
2025-03-28 15:20:44 -0400

    implementation for create endpoint

--- src/main/java/com/mindex/challenge/controller/CompensationController.java
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mindex.challenge.data.Compensation;
@@ -24,7 +26,7 @@ public class CompensationController {
     public ResponseEntity<?> read(@PathVariable String employeeId) {
         try {
             employeeService.read(employeeId);
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             return new ResponseEntity<>("No employee with id: " + employeeId + " found", HttpStatus.NOT_FOUND);
         }
 
@@ -37,5 +39,42 @@ public class CompensationController {
             return ResponseEntity.internalServerError().body("Internal server error:" + e.getMessage());
         }
     }
-    
+
+    @PostMapping("/compensation")
+    public ResponseEntity<?> create(@RequestBody Compensation compensation) {
+        switch(validateCompensation(compensation)) {
+            case -1 -> { return ResponseEntity.badRequest().body("Compensation is missing required values."); }
+            case -2 -> { return ResponseEntity.badRequest().body("Salary must not be a negative number."); }
+        }
+
+        try {
+            employeeService.read(compensation.getEmployeeId());
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>("No employee with id: " + compensation + " found", HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            Compensation createdCompensation = compensationService.create(compensation);
+            return new ResponseEntity<>(createdCompensation, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Internal server error:" + e.getMessage());
+        }
+    }
+
+    // Compensation is only valid if this returns an int > 0
+    private int validateCompensation(Compensation compensation) {
+        Object[] requiredNotNull = {
+            compensation.getEmployeeId(), 
+            compensation.getSalary(),
+            compensation.getEffectiveDate()
+        };
+
+        for(Object req : requiredNotNull) {
+            if(req == null) { return -1; }
+        }
+
+        return (compensation.getSalary() < 0 ? -2 : 1); // returns -2 if salary is negative, distinct case vs null values
+    }
 }
